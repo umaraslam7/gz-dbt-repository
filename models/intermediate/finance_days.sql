9f91d33c954e31023ad55f16e98662e22ce6b9f7
Umar Aslam <umaraslam5107@gmail.com>
2024-04-16 20:08:14 +0000

    Mart model for finance  team

---
+++ models/intermediate/finance_days.sql
@@ -0,0 +1,12 @@
+SELECT 
+    date_date
+    , COUNT(orders_id) AS nb_transactions
+    , ROUND(SUM(revenue),2) AS revenue
+    , ROUND(AVG(revenue),2) AS average_basket
+    , ROUND(SUM(margin),2) AS margin
+    , ROUND(SUM(operational_margin),2) AS operational_margin
+FROM {{ref("int_orders_operational")}}
+GROUP BY date_date
+ORDER BY date_date DESC
+
+    
