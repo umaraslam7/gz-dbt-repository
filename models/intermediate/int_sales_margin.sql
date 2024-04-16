e4340585bd7c4458ad54e3398fed9fc98191dd4a
Umar Aslam <umaraslam5107@gmail.com>
2024-04-16 15:59:14 +0000

    operational margin calculated

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,10 @@
 SELECT
-    (sales.revenue - products.purchase_price) AS margin
+    sales.date_date AS date_date
+    , sales.orders_id AS orders_id
+    , sales.products_id AS products_id
+    , sales.revenue AS revenue
+    , sales.quantity AS quantity
+    , (sales.revenue - (sales.quantity * products.purchase_price)) AS margin
     , (sales.quantity * products.purchase_price) AS purchase_cost
 FROM {{ref('stg_raw_sales')}} AS sales
 LEFT JOIN {{ref('stg_raw_product')}} AS products
--- models/staging/gz_raw_data/stg_raw_ship.sql
+++ models/staging/gz_raw_data/stg_raw_ship.sql
@@ -2,5 +2,5 @@ SELECT
     orders_id
     , shipping_fee
     , logCost AS log_cost
-    , ship_cost
+    , CAST(ship_cost AS FLOAT64) AS ship_cost
 FROM {{source('gz_raw_data', 'raw_gz_ship')}}
